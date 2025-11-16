40474371cf00963bfd07074ce222fb0c25e1d5d4
JaviLopezG <github@javierlopezgonzalez.es>
2025-11-16 20:43:09 +0100

    Don't require normal user for install/uninstall.

--- uninstall.sh
+++ uninstall.sh
@@ -9,9 +9,12 @@ CONFIG_DIR=~/.yups
 
 # --- 1. Root Check ---
 if [ "$EUID" -eq 0 ]; then
-  echo "ERROR: Do not run this script as root."
+  echo "WARNING: Do not run this script as root."
   echo "Run it as your normal user. It will ask for 'sudo' when needed."
-  exit 1
+  read -p "Do you want to continue? (Y/n): " choice
+  if [[ "$choice" == "n" || "$choice" == "N" ]]; then
+      exit 1
+  fi
 fi
 
 echo "Uninstalling YUPS..."
